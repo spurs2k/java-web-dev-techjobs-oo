1e84c2fabc622e26e2226a8380f068cd7726ab36
spurs2k <spurlinjames@gmail.com>
2021-02-12 21:27:35 -0600

    Contemplating how to Test

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -1,6 +1,18 @@
 package org.launchcode.techjobs_oo.Tests;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.launchcode.techjobs_oo.*;
+
 public class JobTest {
+
+    Job emptyJobOne;
+    Job emptyJobTwo;
+    Job testJobOne;
+    Job emptyFieldJob;
+    Job jobEqualityOne;
+    Job JobEqualityTwo;
 
     @Test
     public void testSettingJobId() {
